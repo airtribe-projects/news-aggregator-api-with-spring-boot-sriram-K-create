503abbab0667186385f61a05b309e16d1ae2ae69
SRIRAM K <67730995+sriram-K-create@users.noreply.github.com>
2025-07-05 13:28:44 +0530

    Update AuthController.java

--- src/main/java/com/example/newsapi/controller/AuthController.java
+++ src/main/java/com/example/newsapi/controller/AuthController.java
@@ -1,1 +1,29 @@
 // TODO: Implement AuthController.java
+// TODO: Implement AuthController.java
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+    @Autowired private AuthenticationManager authManager;
+    @Autowired private UserRepository userRepository;
+    @Autowired private JwtUtil jwtUtil;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody @Valid RegisterRequest req) {
+        if (userRepository.findByUsername(req.getUsername()).isPresent()) {
+            return ResponseEntity.badRequest().body("Username already taken");
+        }
+        User user = new User();
+        user.setUsername(req.getUsername());
+        user.setEmail(req.getEmail());
+        user.setPassword(new BCryptPasswordEncoder().encode(req.getPassword()));
+        userRepository.save(user);
+        return ResponseEntity.ok("User registered");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody @Valid LoginRequest req) {
+        authManager.authenticate(new UsernamePasswordAuthenticationToken(req.getUsername(), req.getPassword()));
+        String token = jwtUtil.generateToken(req.getUsername());
+        return ResponseEntity.ok(new JwtResponse(token, req.getUsername()));
+    }
+}
