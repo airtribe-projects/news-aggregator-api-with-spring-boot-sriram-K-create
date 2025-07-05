925e6e5b2d16550f2ad3d6c95a5fcc165620090a
SRIRAM K <67730995+sriram-K-create@users.noreply.github.com>
2025-07-05 13:30:57 +0530

    Update LoginRequest.java

--- src/main/java/com/example/newsapi/dto/LoginRequest.java
+++ src/main/java/com/example/newsapi/dto/LoginRequest.java
@@ -1,1 +1,6 @@
 // TODO: Implement LoginRequest.java
+// TODO: Implement LoginRequest.java
+public class LoginRequest {
+    @NotBlank private String username;
+    @NotBlank private String password;
+}
