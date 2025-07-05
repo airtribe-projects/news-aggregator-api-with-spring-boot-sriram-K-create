2435d6e48ec0990b60e27573de03e5512ffd63e7
SRIRAM K <67730995+sriram-K-create@users.noreply.github.com>
2025-07-05 13:33:00 +0530

    Update UserRepository.java

--- src/main/java/com/example/newsapi/repository/UserRepository.java
+++ src/main/java/com/example/newsapi/repository/UserRepository.java
@@ -1,1 +1,5 @@
 // TODO: Implement UserRepository.java
+// TODO: Implement UserRepository.java
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
