ce1b31a32dded46e1e41ebad0e01f0f81b57b51d
SRIRAM K <67730995+sriram-K-create@users.noreply.github.com>
2025-07-05 13:29:39 +0530

    Update PreferencesController.java

--- src/main/java/com/example/newsapi/controller/PreferencesController.java
+++ src/main/java/com/example/newsapi/controller/PreferencesController.java
@@ -1,1 +1,27 @@
 // TODO: Implement PreferencesController.java
+// TODO: Implement PreferencesController.java
+@RestController
+@RequestMapping("/api/preferences")
+public class PreferencesController {
+    @Autowired private UserRepository userRepository;
+    @Autowired private JwtUtil jwtUtil;
+
+    private User getCurrentUser(HttpServletRequest request) {
+        String token = request.getHeader("Authorization").substring(7);
+        String username = jwtUtil.extractUsername(token);
+        return userRepository.findByUsername(username).orElseThrow();
+    }
+
+    @GetMapping
+    public ResponseEntity<String> getPreferences(HttpServletRequest request) {
+        return ResponseEntity.ok(getCurrentUser(request).getPreferencesJson());
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updatePreferences(HttpServletRequest request, @RequestBody String prefsJson) {
+        User user = getCurrentUser(request);
+        user.setPreferencesJson(prefsJson);
+        userRepository.save(user);
+        return ResponseEntity.ok("Preferences updated");
+    }
+}
